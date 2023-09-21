197656a348a7634cbe360a255fc682d8c1256f9b
Pedro-Paulo-Gomes-Cunha <pedrogomesskeell@gmail.com>
2023-09-21 13:16:56 +0100

    Update 4

--- Domain/Entity/User.cs
+++ Domain/Entity/User.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Dtos;
+using Domain.Interfaces.IEntities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Entity
 {
-    public class User
+    public class User : IUser
     {
         public int Id { get; set; }
         public string Name { get; set; }
---
+++ Domain/Interfaces/IEntities/IUser.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.IEntities
+{
+    public interface IUser
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Password { get; set; }
+
+        UserDto ToDto();
+    }
+}
